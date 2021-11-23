98c1dbe52c4a0dcc7761281d57172f122b02aa82
Sergio Di Pietro <usr2264@salesianos-lacuesta.net>
2021-11-23 12:38:48 +0000

    Feature: Creado método put() de la clase Monitor, el cual sirve para añadir un vegetal al huerto.

--- src/com/dipisoft/farm/Monitor.java
+++ src/com/dipisoft/farm/Monitor.java
@@ -1,4 +1,29 @@
 package com.dipisoft.farm;
 
 public class Monitor {
+
+    // Attribs
+    private String farm[];
+    private int current = 0;
+    private boolean full = false;
+    private boolean empty = true;
+
+    // Constructors
+    public Monitor(int farmSize) {
+        this.farm = new String[farmSize];
+    }
+
+    // Methods
+    public synchronized void put(String vegetable) throws InterruptedException {
+        while (full) wait();
+
+        System.out.println("[ Total de vegetales " + current + "]");
+        // Posible implementación: imprimir representación gráfica del huerto (tabla)
+        // que muestre una tabla con cada posición y cada vegetal
+        farm[current] = vegetable;
+        current +=1;
+        empty = false;
+        full = (current >= farm.length);
+        notifyAll();
+    }
 }
