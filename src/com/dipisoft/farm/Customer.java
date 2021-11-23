c6799d6c624fccc5285bcc9be08b5a4acb487456
Sergio Di Pietro <usr2264@salesianos-lacuesta.net>
2021-11-23 12:46:47 +0000

    Feature: Generadas clases Farmer y Customer, las cuales producirán y consumirán vegetales del huerto.

--- src/com/dipisoft/farm/Customer.java
+++ src/com/dipisoft/farm/Customer.java
@@ -1,4 +1,24 @@
 package com.dipisoft.farm;
 
-public class Customer {
+public class Customer extends Thread {
+    // Attribs
+    private Monitor farm;
+
+    // Constructors
+    public Customer(Monitor farm) {
+        this.farm = farm;
+    }
+
+    // Methods
+    @Override
+    public void run() {
+        String vegetable;
+
+        try {
+            vegetable = farm.get();
+            System.out.println("(-) Se ha vendido el vegetal: " + vegetable);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/dipisoft/farm/Farmer.java
+++ src/com/dipisoft/farm/Farmer.java
@@ -1,4 +1,24 @@
 package com.dipisoft.farm;
 
-public class Farmer {
+public class Farmer extends Thread {
+    // Attribs
+    private Monitor farm;
+
+    // Constructors
+    public Farmer(Monitor farm) {
+        this.farm = farm;
+    }
+
+    // Methods
+    @Override
+    public void run() {
+        String vegetable = "lechuga";
+
+        try {
+            farm.put(vegetable);
+            System.out.println("(+) Se ha producido el vegetal: " + vegetable);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
